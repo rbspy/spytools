3e78bcc4936d0f62d2bf17d6c482ef5edf7c81a7
Adam Jensen <adam@acj.sh>
2023-12-31 07:59:35 -0500

    CI: Upgrade to FreeBSD 14 and faster virtualization

--- ci/freebsd-build.sh
+++ ci/freebsd-build.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
 
-source ~/.bash_profile
-
 set -e
 
-ruby -v
-cargo --version
-
-sudo pkg install -y llvm
-
-export CARGO_HOME="/vagrant/.cargo"
-mkdir -p $CARGO_HOME
+source "$HOME/.cargo/env"
 
 cd /vagrant
 
-if [ -f build-artifacts.tar ]; then
-  tar xf build-artifacts.tar
-  rm -f build-artifacts.tar
-fi
-
-cargo build --release
-cargo test --release
-
-tar cf build-artifacts.tar target
-tar rf build-artifacts.tar .cargo/git || true
-tar rf build-artifacts.tar .cargo/registry || true
+cargo build --no-default-features --release --workspace --all-targets
+cargo test --no-default-features --release
